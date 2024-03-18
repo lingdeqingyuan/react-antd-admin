71231fd2f212bef1ab3bb92c2436ea420d2f29b3
ryuji <yrejgnl11@163.com>
2024-03-18 21:06:53 +0800

    chore: prettier config

---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: false,
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  trailingComma: 'none',
+};
